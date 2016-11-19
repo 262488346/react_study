43be637c30f1c6f22fc33d5eb9e4ed33ff8ee5d9
刘刚 <262488346@qq.com>
2016-11-19 21:28:47 +0800

    no message

--- reactDemo2/zoom.js
+++ reactDemo2/zoom.js
@@ -9,6 +9,15 @@ var style1 = {
     "padding-top":"20px",
     "margin":"10px"
 };
+
+var styleHide = {
+    "width": "50px",
+    "height":"30px;",
+    "background":"#f00",
+    "text-align":"center",
+    "line-height":"30px",
+    "color":"#fff"
+};
 var style2 = {
     "color":"#ccc",
     "font-size":"40px",
@@ -32,7 +41,10 @@ var ZoomInner = React.createClass({
             style = style2
         }
         return (
-            <div style={style}>{this.props.val}</div>
+            <div>
+                <div style={style}>{this.props.val}</div>
+                <div style={styleHide} onClick={this.props.zoomHide}>隐藏</div>
+            </div>
         )
     }
 });
@@ -45,17 +57,24 @@ var ZoomInput = React.createClass({
     },
     changeText: function(e){
         this.setState({
-            zoom:e.target.value,
             val: e.target.value
         });
     },
+    zoomHide: function(){
+        this.setState({
+            zoom:2
+        })
+    },
+
     render: function () {
         var zoomInner;
-
+        if(this.state.zoom == 1){
+            zoomInner = <ZoomInner val={this.state.val}  zoomHide={this.zoomHide}/>;
+        }
         return (
             <div>
                 <input type="text" placeholder="请输入文字" onChange={this.changeText}/>
-                 <ZoomInner val={this.state.val}/>
+                {zoomInner}
             </div>
         )
     }
